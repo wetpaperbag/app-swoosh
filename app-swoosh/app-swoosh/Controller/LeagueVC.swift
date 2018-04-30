8ccd13a2de06f47bf10e861311309ec7f1b31c7f
Junior <Junior@Family-MacBook.local>
2018-04-30 16:44:51 -0700

    added ibfunctions for player type, made next button unselectable until a league is selected.

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,18 +10,36 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    @IBOutlet weak var nextBtn: BorderButton!
     
+    @IBAction func onMensTapped(_ sender: Any) {
+       selectLeague(leagueType: "mens")
+        
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
     
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+    player.desiredLeague = leagueType
+    nextBtn.isEnabled = true
+    }
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Junior on 4/30/18.
+//  Copyright © 2018 Junior. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selecte3dSkillLevel: String?
+    
+}
