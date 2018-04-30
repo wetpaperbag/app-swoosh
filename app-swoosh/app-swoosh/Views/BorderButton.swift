e712c9a4d721a5ec9bcb2dab28949096c838987b
Junior <Junior@Family-MacBook.local>
2018-04-30 01:11:44 -0700

    Setup welcome screen

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Junior on 4/30/18.
+//  Copyright © 2018 Junior. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
